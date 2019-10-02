6af0616d3e0da3b26b0481133570f4066e4a8be2
James Shelton <jshelton140@googlemai.com>
2019-10-02 17:58:50 +0100

    Added Output Class, Added Run and Play methods to MafiaGame.

--- Mafia/src/MafiaGame.java
+++ Mafia/src/MafiaGame.java
@@ -40,12 +40,77 @@ public class MafiaGame {
 		
 	}
 	
+	/*
+	 * Menu Logic
+	 */
+	
+	public void run() {
+		 Boolean programRunning = true;
+		 Scanner input = new Scanner(System.in);
+		 
+		 String[] mainMenu = {"Setup", "Play", "Exit"};
+		 
+		 while(programRunning == true) {
+			 
+			 Output.printMenu(mainMenu);
+			 
+			 int selectedOption = 0;
+			 selectedOption = input.nextInt();
+			 
+			 switch (selectedOption) {
+			 	case 1:
+			 		//setup();
+			 		break;
+			 		
+			 	case 2:
+			 		play();
+			 		break;
+			 		
+			 	case 3:
+			 		break;
+			 		
+			 	case 0:
+			 		programRunning = false;
+			 		break;
+			 		
+			 	default:
+			 		System.out.println("Invalid input! Please try again.");
+			 }
+			 
+		 }
+		 input.close();
+	}
+	
+	/*
+	 * Game Logic
+	 */
+	
+	private void play() {
+		
+		Boolean gameNotFinished = true;
+		
+		while(gameNotFinished == true) {
+			
+			
+			System.out.println("We played a game!");
+			/*
+			 *  End game to stop infinite loop.
+			 */
+			gameNotFinished = false;
+		}
+	}
+	
+	
+	/*
+	 * Main Method
+	 */
 	
 	public static void main(String[] args) {
 		
 		MafiaGame game = new MafiaGame();
-		System.out.println(game.getPlayerList());
+		System.out.print(game.getPlayerList()+"\n");
 		System.out.println(game.getPlayerIndexList());
+		game.run();
 	}
 
 }
---
+++ Mafia/src/Output.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Output {
+	
+	public static void printMenu(String[] menu) {
+		//System.out.println(Arrays.toString(menu));
+		
+		for(int i = 0; i < menu.length; i++) {
+			if(menu[i] != "Exit") {
+				System.out.println(i+1+". "+menu[i]);
+			} else {
+				System.out.println(0+". "+menu[i]);
+			}
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
